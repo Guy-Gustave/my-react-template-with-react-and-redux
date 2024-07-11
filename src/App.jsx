f994f30a0bfb09b9d2d70a6e34e388fc10e33e42
Guy-Gustave <guymoustapha@gmail.com>
2024-07-11 04:52:30 +0200

    Initial setup with vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <h1>welcome to my app</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
